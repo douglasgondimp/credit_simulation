ed8974233325a039be1bb754be48a59332ee0e77
Douglas Gondim <douglasgondimp@gmail.com>
2022-05-20 13:35:28 -0300

    Inserção das rotas de busca por instituicoes e convênios no arquivo routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ConveniosController;
+use App\Http\Controllers\InstituicoesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/institutions', [InstituicoesController::class, 'index']);
+Route::get('/covenants',    [ConveniosController::class, 'index']);
