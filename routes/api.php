c38c2522bfb525af19a8511641cb199ff3076360
Douglas Gondim <douglasgondimp@gmail.com>
2022-05-20 16:40:33 -0300

    Inclusão da rota para simular o crédito

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ConveniosController;
 use App\Http\Controllers\InstituicoesController;
+use App\Http\Controllers\SimulacaoCreditoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/institutions', [InstituicoesController::class, 'index']);
-Route::get('/covenants',    [ConveniosController::class, 'index']);
+Route::group([
+    'middleware' => 'api'
+], function () {
+    Route::get('/institutions',       [InstituicoesController::class, 'index']);
+    Route::get('/covenants',          [ConveniosController::class, 'index']);
+    Route::post('/simulation-credit', [SimulacaoCreditoController::class, 'creditSimulation']);
+});
