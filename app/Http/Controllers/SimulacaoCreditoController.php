1bdb16f5d6989862f8829d845985b4f6ec34402d
Douglas Gondim <douglasgondimp@gmail.com>
2022-05-20 13:34:23 -0300

    Criação do arquivo SimulacaoCreditoController

---
+++ app/Http/Controllers/SimulacaoCreditoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SimulacaoCreditoController extends Controller
+{
+    public function creditSimulation()
+    {
+        
+    }
+}
